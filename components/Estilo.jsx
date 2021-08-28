76ba99e8df12f16f9e00107a246408f8e8d1a324
Kleiton Bezerra <kleiton2102@gmail.com>
2021-08-28 17:59:49 -0300

    Usando style e classe nos componentes e correção de lint

--- components/Estilo.jsx
+++ components/Estilo.jsx
@@ -1,9 +1,22 @@
 export default function Estilo(props) {
+  const classeAplicada = props.numero >= 0 ? "azul" : "vermelho";
+
   return (
     <div>
-      <h1 style={{ backgroundColor: props.numero >= 0 ? "#2d2" : "#d22" }}>
-        Texto
+      <h1
+        style={{
+          backgroundColor: props.numero >= 0 ? "#2d2" : "#d22",
+          color: props.color,
+          padding: "10px",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: props.centro ? "center" : "flex-start",
+        }}
+      >
+        Texto #01
       </h1>
+
+      <h2 className={classeAplicada}>Texto #02</h2>
 
       <p style={{ color: props.nota >= 7 ? "blue" : "red" }}>{props.nota}</p>
     </div>
--- pages/css/usandoEstilo.jsx
+++ pages/css/usandoEstilo.jsx
@@ -3,10 +3,10 @@ import Estilo from "../../components/Estilo";
 export default function usandoEstilo() {
   return (
     <div>
-      <Estilo numero={3} />
-      <Estilo numero={-13} />
-      <Estilo nota={8} />
-      <Estilo nota={4} />
+      <Estilo numero={3} color="#000" />
+      <Estilo numero={-13} color="#fff" centro />
+      {/* <Estilo nota={8} />
+      <Estilo nota={4} /> */}
     </div>
   );
 }

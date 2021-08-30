6ac2880463449ff6e27feb21a04f1727c7e9e9bb
Kleiton Bezerra <kleiton2102@gmail.com>
2021-08-30 14:36:48 -0300

    Repetição #01

---
+++ pages/render/repeticao1.jsx
@@ -0,0 +1,28 @@
+export default function repeticao1() {
+  const listaAprovados = [
+    "João",
+    "Maria",
+    "Ana",
+    "Bia",
+    "Carlos",
+    "Daniel",
+    "Laura",
+  ];
+
+  function renderizarLista() {
+    return listaAprovados.map((nome, i) => <li key={i}>{nome}</li>);
+  }
+
+  return <ul>{renderizarLista()}</ul>;
+}
+
+// Alternativa com for para percorrer um Array
+// function renderizarLista() {
+//   const itens = [];
+
+//   for (let i = 0; i < listaAprovados.length; i++) {
+//     itens.push(<li key={i}>{listaAprovados[i]}</li>);
+//   }
+
+//   return itens;
+// }

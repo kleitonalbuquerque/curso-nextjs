3e1fc5d0870f320efc97323a3ecebf5aa0e9f05e
kleitonalbuquerque <kleiton2102@gmail.com>
2021-10-04 17:00:56 -0300

    Componente baseado em Classe

---
+++ components/Contador.jsx
@@ -0,0 +1,53 @@
+import { Component } from "react";
+
+export default class Contador extends Component {
+  // estado
+  state = {
+    numero: this.props.valorInicial ?? 0,
+    passo: this.props.passo ?? 1,
+  };
+
+  // métodos
+  inc = () => {
+    this.setState({
+      numero: this.state.numero + this.state.passo,
+    });
+  };
+
+  dec = () => {
+    this.setState({
+      numero: this.state.numero - 1,
+    });
+  };
+
+  alterarPasso = (e) => {
+    this.setState({ passo: +e.target.value });
+  };
+
+  // funções
+  renderForm() {
+    return (
+      <>
+        <input
+          type="number"
+          min={1}
+          max={1000}
+          value={this.state.passo}
+          onChange={this.alterarPasso}
+        />
+        <button onClick={this.inc}>+</button>
+        <button onClick={this.dec}>-</button>
+      </>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Contador (usando Classe)</h1>
+        <h2>{this.state.numero}</h2>
+        {this.renderForm()}
+      </div>
+    );
+  }
+}
